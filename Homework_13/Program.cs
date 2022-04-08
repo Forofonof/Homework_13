a45cc225b69125baeb7717d4045b2c818955401e
Forofonof <DanielResult1@outlook.com>
2022-04-08 18:44:32 +0300

    Добавьте файлы проекта.

---
+++ Homework_13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string message;
+        int messageLength;
+        string symbol;
+
+        Console.WriteLine("Введите слово:");
+        message = Console.ReadLine();
+        messageLength = message.Length;
+        Console.WriteLine("Введите символ:");
+        symbol = Console.ReadLine();
+
+        for (int i = 0; i < messageLength + 2; i++)
+        {
+            Console.Write(symbol);
+        }
+
+        Console.WriteLine($"\n{symbol}{message}{symbol}");
+
+        for (int i = 0; i < messageLength + 2; i++)
+        {
+            Console.Write(symbol);
+        }
+    }
+}
