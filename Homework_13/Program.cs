60ce249175567e37549175ee2d850fe56252a274
Forofonof <DanielResult1@outlook.com>
2022-04-09 12:31:26 +0300

    FIX_1.6

--- Homework_13/Program.cs
+++ Homework_13/Program.cs
@@ -4,23 +4,25 @@
 {
     static void Main(string[] args)
     {
-        string message;
-        int messageLength;
+        string name;
+        int nameLength;
+        int addToLength = 1;
         string symbol;
         string symbolFrame = "";
 
-        Console.WriteLine("Введите слово:");
-        message = Console.ReadLine();
-        messageLength = message.Length;
+        Console.WriteLine("Введите имя:");
+        name = Console.ReadLine();
+        nameLength = name.Length;
         Console.WriteLine("Введите символ:");
         symbol = Console.ReadLine();
 
-        for (int i = -1; i <= messageLength; i++)
+        for (int i = 0; i <= nameLength + addToLength; i++)
         {
             symbolFrame += symbol;
         }
+
         Console.WriteLine(symbolFrame);
-        Console.WriteLine(symbol + message + symbol);
+        Console.WriteLine(symbol + name + symbol);
         Console.WriteLine(symbolFrame);
     }
 }
